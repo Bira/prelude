51f7af46ac5949f37dbdb6dc69d5d152d3783959
Ubiratan Pires Alberton <u.alberton@gmail.com>
2013-07-28 20:44:33 -0300

    Personal customizations.
    A few additional personal changes.
    
    Let me choose my own theme, thank you very much.
    
    Use solarized-light as the default theme.
    
    Require elixir-mode
    
    Font size 13 for a 1920x1080 display.
    
    Set font size according to display size.
    
    Invert order of operands here to get the result we want.
    
    It turns out the reason prelude-ruby wasn't working is that I didn't enable it.
    
    Small tweaks.
    
    Configurations to JS modules.
    
    Configurations to JS modules.
    
    Don't use whitespace mode in HTML files.
    
    Prelude modules I'm using in my personal branch
    
    No flyspell by default
    
    Tweaks after a Prelude update
    
    Let me choose my own theme, thank you very much.
    
    Satsuki-specific branch
    
    Satsuki-specific changes
    
    A couple more updates
    
    Fix Prelude theming
    
    Use Hack font since we're on Ubuntu 16.04+
    
    Reformat options and disable some unwanted ido functionality
    
    Set the correct option for indenting JS code.
    
    Minor corrections
    
    More minor adjustments

--- modules/prelude-js.el
+++ modules/prelude-js.el
@@ -46,6 +46,7 @@
     ;; electric-layout-mode doesn't play nice with smartparens
     (setq-local electric-layout-rules '((?\; . after)))
     (setq mode-name "JS2")
+    (setq-local c-basic-offset 2)
     (js2-imenu-extras-mode +1))
 
   (setq prelude-js-mode-hook 'prelude-js-mode-defaults)

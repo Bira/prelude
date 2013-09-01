4c83b82d0fd4d797848edb7ba8f4188f6d1d5939
Ubiratan Pires Alberton <u.alberton@gmail.com>
2013-09-01 20:45:53 -0300

    A few additional personal changes.

--- personal/bira.el
+++ personal/bira.el
@@ -2,11 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 (global-linum-mode)
-(prelude-ensure-module-deps '(rbenv rubocop ruby-hash-syntax))
+(prelude-ensure-module-deps '(go-mode rbenv rubocop ruby-hash-syntax))
+(if (file-exists-p "/opt/boxen/rbenv") (setq rbenv-installation-dir "/opt/boxen/rbenv"))
 (global-rbenv-mode)
 (if (window-system) (server-start))
-(set-frame-font "Source Code Pro-13")
+(set-frame-font "Source Code Pro-18")
 (setq exec-path (cons (getenv "PATH") exec-path))
+(global-set-key "\M-g" 'goto-line)
+(add-hook 'ruby-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "RET") 'newline-and-indent)
+             (whitespace-mode)
+             (flycheck-mode)))
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 (setq prelude-guru nil)
+(define-key global-map [home] 'beginning-of-line)
+(define-key global-map [end] 'end-of-line)
+(setq-default fill-column 80)
 (provide 'bira)
 ;;; bira.el ends here

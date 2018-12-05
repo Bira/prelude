6411be21a7943d2cde17e0f7315c9b1c09d8b4e5
Ubiratan Pires Alberton <u.alberton@gmail.com>
2018-12-06 10:35:25 +1100

    Theme update and prettier-js

--- personal/bira.el
+++ personal/bira.el
@@ -22,6 +22,8 @@
 (add-to-list 'auto-mode-alist '("\\.prawn\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.slm\\'" . slim-mode))
 (add-hook 'html-mode-hook (lambda() (whitespace-mode -1)))
+(add-hook 'web-mode-hook #'add-node-modules-path)
+(add-hook 'web-mode-hook #'prettier-js-mode)
 (setq prelude-guru nil
       prelude-auto-save nil
       ido-use-filename-at-point nil

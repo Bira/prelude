2c26cc0ac85827d3e4d7632e0fc14478cbb33407
Ubiratan Pires Alberton <u.alberton@gmail.com>
2020-05-09 21:41:35 -0300

    A few satsuki updates

--- personal/bira.el
+++ personal/bira.el
@@ -9,7 +9,9 @@
                             prettier-js
                             rbenv
                             rubocop
-                            ruby-hash-syntax))
+                            ruby-hash-syntax
+                            slim-mode
+                            web-mode))
 (global-rbenv-mode)
 (if (window-system) (server-start))
 (set-frame-font "Hack")
@@ -22,20 +24,25 @@
 (add-hook 'gfm-mode-hook 'turn-on-auto-fill)
 (add-hook 'rust-mode-hook (lambda()
                             (flycheck-mode -1)))
+(setq-default flycheck-disabled-checkers
+              (append flycheck-disabled-checkers
+                      '(javascript-jshint json-jsonlist)))
+(flycheck-add-mode 'javascript-eslint 'rjsx-mode)
 (add-to-list 'auto-mode-alist '("\\.prawn\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.slm\\'" . slim-mode))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 (add-hook 'html-mode-hook (lambda() (whitespace-mode -1)))
-(add-hook 'web-mode-hook #'add-node-modules-path)
-(add-hook 'web-mode-hook #'prettier-js-mode)
+(add-hook 'rjsx-mode-hook #'add-node-modules-path)
+(add-hook 'rjsx-mode-hook #'prettier-js-mode)
+(add-hook 'rjsx-mode-hook (lambda() (whitespace-mode -1)))
 (setq prelude-guru nil
       prelude-auto-save nil
-      ido-use-filename-at-point nil
-      ido-use-url-at-point nil
+      lsp-enable-snippet nil
       ruby-use-smie t
       ruby-deep-arglist nil
       ruby-deep-indent-paren nil
-      ruby-insert-encoding-magic-comment nil)
+      ruby-insert-encoding-magic-comment nil
+      js2-strict-missing-semi-warning nil)
 (define-key global-map [home] 'beginning-of-line)
 (define-key global-map [end] 'end-of-line)
 (setq-default fill-column 80)

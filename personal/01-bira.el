2e617034ff18f9b9238d666422f0713beb9d98c5
Ubiratan Pires Alberton <Bira@users.noreply.github.com>
2024-08-03 17:08:25 -0300

    Bring in useful changes from Satsuki branch

--- personal/01-bira.el
+++ personal/01-bira.el
@@ -3,12 +3,12 @@
 (setq-default fill-column 80)
 (super-save-mode -1)
 (setq require-final-newline t
-      prelude-guru nil)
+      prelude-guru nil
+      prelude-flyspell nil)
+
 
 (setq default-frame-alist '((font . "Monaspace Neon-12")))
 (global-set-key "\M-g" 'goto-line)
-
-(if (window-system) (server-start))
 
 ;; Theme
 
@@ -45,11 +45,17 @@
 
 (prelude-require-package 'rubocop)
 
+;; Rust development
+
+(add-hook 'rust-mode-hook (lambda()
+                            (setq whitespace-line-column 100)))
+
 ;; Front-end development
 
 (prelude-require-packages '(slim-mode
                             rjsx-mode
                             prettier-js))
+
 (add-to-list 'auto-mode-alist '("\\.slm\\'" . slim-mode))
 (add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
 
@@ -57,9 +63,27 @@
               (append flycheck-disabled-checkers
                       '(javascript-jshint json-jsonlist)))
 
+(setq js2-strict-missing-semi-warning nil)
+
 (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
+(flycheck-add-mode 'javascript-eslint 'js2-mode)
+
+
+;; use local eslint from node_modules before global
+;; http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
+(defun my/use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint (and root
+                      (expand-file-name "node_modules/eslint/bin/eslint.js"
+                                        root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
+
 (add-hook 'rjsx-mode-hook (lambda() (whitespace-mode -1)))
-(add-hook 'rjsx-mode-hook #'add-node-modules-path)
+;; (add-hook 'rjsx-mode-hook #'add-node-modules-path)
 (add-hook 'rjsx-mode-hook #'prettier-js-mode)
 
 (add-hook 'slim-mode-hook (lambda ()
@@ -68,3 +92,5 @@
 
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 (add-hook 'markdown-mode-hook 'turn-on-auto-fill)
+
+(server-start)
